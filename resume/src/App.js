6645676e6b2c9a1a4176ada6a251c52a938613f1
Misha <mishaaisyah30@yahoo.com>
2024-09-22 14:17:30 +0800

    Adding Avatar & files for other components

--- resume/src/App.js
+++ resume/src/App.js
@@ -1,12 +1,23 @@
 
 import './App.css';
+import Avatar from '@mui/material/Avatar';
+import Picture from './picture.png';
+// import Achievements from './Achievements.js';
+// import Education from './Education.js';
+// import Experience from './Experience.js';
+// import Skills from './Skills.js';
+
 
 function App() {
   return (
     <div className="App">
-      <div className="content">
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', paddingTop: "4vh"}}>
+      <Avatar alt="Misha" src={Picture} sx={{ width: 150, height: 150 }}/>
+      </div>
+      <div className="Name">
         <h1>Misha Aisyah binti Mohd Mokhdhari</h1>
       </div>
+      
     </div>
   );
 }
