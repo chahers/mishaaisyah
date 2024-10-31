3b558cec2b637f56519b63f49dcea0fe6077aa10
Misha <mishaaisyah30@yahoo.com>
2024-10-31 15:31:58 +0800

    changed favicon, and added components in the home page

--- resume/src/Home.js
+++ resume/src/Home.js
@@ -1,9 +1,15 @@
 import './Home.css';
-
-function Home() {
+import picture from './rayapic2024.jpeg';
+const Home = () => {
     return ( 
         <>
-            
+            <h1 className='myheading'>Hello World, I'm Misha!</h1>
+            <div className='image-content'>
+            <img src={picture} alt="Misha" className="profile-image" />
+                <div className="description">
+                    <p>Welcome to my personal webpage! I'm a recent Lancaster University Software Engineering graduate. Scroll down to know more about me!</p>
+                </div>
+            </div>
         </>
      );
 }
