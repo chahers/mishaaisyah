0d29798546276da9524c051b0ea495ab0c5dff79
Misha <mishaaisyah30@yahoo.com>
2024-11-01 16:42:12 +0800

    fixed positions

--- resume/src/skill/Skill.js
+++ resume/src/skill/Skill.js
@@ -1,15 +1,30 @@
 import './Skill.css';
+import reactIcon from './react.svg';
+import htmlIcon from './html.svg';
+import cssIcon from './css.svg';
+import jsIcon from './js.svg';
+import githubIcon from './github.svg';
+import pythonIcon from './python.svg';
+import javaIcon from './java.svg';
+import cIcon from './c-programming.svg';
+import mysqlIcon from './mysql.svg';
+import postgresqlIcon from './postgresql.svg';
 
 const Skill = () => {
     return ( 
         <>
+        <div className='skill-wrapper'>
             <div className='skill-container'>
                 <h1 className='skill-heading'>Applicable skills</h1>
                 <div>
                     <p className='skill-description'>These are the tools and technologies I have worked with during my studies in Lancaster University.</p>
                 </div>
+                <ul className='apps'>
+
+                </ul>
             </div>
-            <div className='skill-backlay'/>
+        </div>
+        <div className='skill-backlay'/>
         </>
      );
 }
